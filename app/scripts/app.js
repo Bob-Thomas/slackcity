9e640a23b0702f51aa44e0b4c21d42a37b3a7ca0
bob thomas <smartcat007@hotmail.com>
2016-07-30 12:39:10 +0200

    removing stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,9 +20,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+        template: '<quote></quote>'
       })
       .otherwise({
         redirectTo: '/'
